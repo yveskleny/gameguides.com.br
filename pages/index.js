9dc610d7fad36aa60d454bc116e9cce46e2e00c4
Yves Kleny <yvesk.martins@gmail.com>
2025-05-07 18:15:09 -0300

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <>
+      <h1>RESERVADO PARA CONSTRUÇÃO 🚧</h1>
+      <h2>
+        O futuro aqui é sigiloso... mas promete! 🐱‍👤 (Enquanto isso, tenha um
+        ótimo dia!)
+      </h2>
+    </>
+  );
+}
+
+export default Home;
