5a8bad719a5812f8be8cca8be19adc3142f5a03a
Yves Kleny <yvesk.martins@gmail.com>
2025-08-04 15:45:49 -0300

    add `database.js` and `.env.development` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function Home(request, response) {
+import database from "../../../../infra/database.js";
+
+async function Home(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({
     chave: "my api response",
   });
